a781025efc2b4f405f10cc92e915d38ca0cc071f
Arnel Imperial <engine@Arnels-MacBook-Pro.local>
2020-04-05 21:46:26 +0300

    First commit

---
+++ users/managers.py
@@ -0,0 +1,54 @@
+from django.contrib.auth.models import BaseUserManager
+from django.db import models
+from django.utils.translation import ugettext_lazy as _
+
+
+class UserManager(BaseUserManager):
+    """Define a model manager for User model with no username field."""
+
+    use_in_migrations = True
+
+    def _create_user(self, email, first_name, last_name, password, **extra_fields):
+        """Create and save a User with the given email and password."""
+        if not email:
+            raise ValueError(_('Email must be set'))
+        if not first_name:
+            raise ValueError(_('Users must have a first name'))
+        if not last_name:
+            raise ValueError(_('Users must have a last name'))
+
+        email = self.normalize_email(email)
+        user = self.model(email=email, first_name=first_name, last_name=last_name, **extra_fields)
+        user.set_password(password)
+        user.save(using=self._db)
+        return user
+
+    def create_user(self, email, first_name, last_name, password=None, **extra_fields):
+        """Create and save a regular User with the given email and password."""
+        extra_fields.setdefault('is_active', True)
+        extra_fields.setdefault('is_staff', False)
+        extra_fields.setdefault('is_superuser', False)
+        return self._create_user(email, first_name, last_name, password, **extra_fields)
+
+    def create_staffuser(self, email, first_name, last_name, password, **extra_fields):
+        """Create and save a regular User with the given email and password."""
+        extra_fields.setdefault('is_active', True)
+        extra_fields.setdefault('is_staff', True)
+        extra_fields.setdefault('is_superuser', False)
+        return self._create_user(email, first_name, last_name, password, **extra_fields)
+
+    def create_superuser(self, email, first_name, last_name, password, **extra_fields):
+        """Create and save a SuperUser with the given email and password."""
+        extra_fields.setdefault('is_active', True)
+        extra_fields.setdefault('is_staff', True)
+        extra_fields.setdefault('is_superuser', True)
+
+        if extra_fields.get('is_staff') is not True:
+            raise ValueError('Superuser must have is_staff=True.')
+        if extra_fields.get('is_superuser') is not True:
+            raise ValueError('Superuser must have is_superuser=True.')
+
+        return self._create_user(email, first_name, last_name, password, **extra_fields)
+
+
+
